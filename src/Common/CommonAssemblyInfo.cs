b94cec6438b452a7270f3b8fed14715f5d79a1b3
Kevin Obee <kevin.obee@gmail.com>
2013-07-21 10:18:33 +0100

    Common assembly versioning implemented

---
+++ src/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Sitecore")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
+
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+
+[assembly: NeutralResourcesLanguage("en-GB")]
