ee9b701a9d6482dbd4d42e2811f24c40052b8c74
Kevin Obee <kevin.obee@gmail.com>
2013-07-25 18:05:56 +0100

    coupling tests added

---
+++ tests/unit-test/Sitecore.Ship.Test/CouplingTests.cs
@@ -0,0 +1,23 @@
+﻿using TestUtils;
+
+using Xunit;
+
+namespace Sitecore.Ship.Test
+{
+    public class CouplingTests
+    {
+        [Fact]
+        public void Sitecore_Ship_should_not_reference_Sitecore_Kernel()
+        {
+            // Arrange
+            var assemblyType = typeof(Sitecore.Ship.Package.Install.InstallerModule);
+            const string unwantedAssemblyName = "Sitecore.Kernel";
+
+            // Act
+            var containsReference = assemblyType.AssemblyContainsReferencesTo(unwantedAssemblyName);
+
+            // Assert
+            Assert.False(containsReference, string.Format("{0} should not be referenced by Sitecore.Ship", unwantedAssemblyName));
+        }
+    }
+}
