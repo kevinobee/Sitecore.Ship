ab7dd3475377f0c4231ae403eef8a6c19e5de5c5
Kevin Obee <kevin.obee@gmail.com>
2015-08-24 00:29:44 +0100

    Tried to undo weird formatting that carried across from work StyleCop settings

--- src/Sitecore.Ship.AspNet/Publish/InvokePublishingCommand.cs
+++ src/Sitecore.Ship.AspNet/Publish/InvokePublishingCommand.cs
@@ -11,36 +11,36 @@ using Sitecore.Ship.Infrastructure;
 
 namespace Sitecore.Ship.AspNet.Publish
 {
-    public class InvokePublishingCommand : CommandHandler
-    {
-        private readonly IPublishService _publishService;
+	public class InvokePublishingCommand : CommandHandler
+	{
+		private readonly IPublishService _publishService;
 
-        public InvokePublishingCommand(IPublishService publishService)
-        {
-            _publishService = publishService;
-        }
+		public InvokePublishingCommand(IPublishService publishService)
+		{
+			_publishService = publishService;
+		}
 
-        public InvokePublishingCommand() : this(new PublishService()) 
-        {
-        }
+		public InvokePublishingCommand() : this(new PublishService()) 
+		{
+		}
 
-        public override void HandleRequest(HttpContextBase context)
-        {
-            if (CanHandle(context))
-            {
-                var publishParameters = GetRequest(context.Request);
+		public override void HandleRequest(HttpContextBase context)
+		{
+			if (CanHandle(context))
+			{
+				var publishParameters = GetRequest(context.Request);
 
-                var now = DateTime.Now;
-                var date = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+				var now = DateTime.Now;
+				var date = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
 
-                if (publishParameters.Mode.ToLowerInvariant() == "listofitems")
-                {
-                    using (var reader = new StreamReader(context.Request.InputStream))
-                    {
-                        var values = reader.ReadToEnd();
+				if (publishParameters.Mode.ToLowerInvariant() == "listofitems")
+				{
+					using (var reader = new StreamReader(context.Request.InputStream))
+					{
+						var values = reader.ReadToEnd();
 
 						if (string.IsNullOrEmpty(values))
-	                    {
+						{
 							JsonResponse(string.Empty, HttpStatusCode.NoContent, context);
 						}
 						else
@@ -59,50 +59,50 @@ namespace Sitecore.Ship.AspNet.Publish
 								JsonResponse(json, HttpStatusCode.Accepted, context);
 							}							
 						}
-                    }
-                }
-                else
-                {
-                    _publishService.Run(publishParameters);
+					}
+				}
+				else
+				{
+					_publishService.Run(publishParameters);
 					var json = Json.Encode(new { date });
 
 					JsonResponse(json, HttpStatusCode.Accepted, context);
-                }
-            }
-            else if (Successor != null)
-            {
-                Successor.HandleRequest(context);
-            }
-        }
+				}
+			}
+			else if (Successor != null)
+			{
+				Successor.HandleRequest(context);
+			}
+		}
 
-        private static bool CanHandle(HttpContextBase context)
-        {
-            return context.Request.Url != null &&
-                   IsPublishModeUrl(context.Request.Url.PathAndQuery.ToLowerInvariant()) &&
-                   context.Request.HttpMethod == "POST";
-        }
+		private static bool CanHandle(HttpContextBase context)
+		{
+			return context.Request.Url != null &&
+				   IsPublishModeUrl(context.Request.Url.PathAndQuery.ToLowerInvariant()) &&
+				   context.Request.HttpMethod == "POST";
+		}
 
-        private static bool IsPublishModeUrl(string urlPath)
-        {
-            return urlPath.EndsWith("/services/publish/full") ||
-                   urlPath.EndsWith("/services/publish/smart") ||
-                   urlPath.EndsWith("/services/publish/incremental") ||
-                   urlPath.EndsWith("/services/publish/listofitems");
-        }
+		private static bool IsPublishModeUrl(string urlPath)
+		{
+			return urlPath.EndsWith("/services/publish/full") ||
+				   urlPath.EndsWith("/services/publish/smart") ||
+				   urlPath.EndsWith("/services/publish/incremental") ||
+				   urlPath.EndsWith("/services/publish/listofitems");
+		}
 
-        private static PublishParameters GetRequest(HttpRequestBase request)
-        {
-            if (request.Url == null) throw new InvalidOperationException("Missing Url");
+		private static PublishParameters GetRequest(HttpRequestBase request)
+		{
+			if (request.Url == null) throw new InvalidOperationException("Missing Url");
 
-            var publishRequest = new PublishParameters
-                {
-                    Mode = request.Url.PathAndQuery.Split(new[] {'/'}).Last(),
-                    Source = request.Form["source"] ?? "master",
-                    Targets = request.Form["targets"].CsvStringToStringArray(new[] { "web" }),
-                    Languages = request.Form["languages"].CsvStringToStringArray(new[] { "en" })
-                };
+			var publishRequest = new PublishParameters
+				{
+					Mode = request.Url.PathAndQuery.Split(new[] {'/'}).Last(),
+					Source = request.Form["source"] ?? "master",
+					Targets = request.Form["targets"].CsvStringToStringArray(new[] { "web" }),
+					Languages = request.Form["languages"].CsvStringToStringArray(new[] { "en" })
+				};
 
-            return publishRequest;
-        }
-    }
+			return publishRequest;
+		}
+	}
 }
--- src/Sitecore.Ship.Infrastructure/PublishService.cs
+++ src/Sitecore.Ship.Infrastructure/PublishService.cs
@@ -12,19 +12,19 @@ using Sitecore.Ship.Core.Domain;
 
 namespace Sitecore.Ship.Infrastructure
 {
-    public class PublishService : IPublishService
-    {
-        private readonly Dictionary<string, Func<Database, Database[], Language[], Handle>> _publishingActions;
+	public class PublishService : IPublishService
+	{
+		private readonly Dictionary<string, Func<Database, Database[], Language[], Handle>> _publishingActions;
 
-        public PublishService()
-        {
-            _publishingActions = new Dictionary<string, Func<Database, Database[], Language[], Handle>>
-                {
-                    { "full",           Publishing.PublishManager.Republish },
-                    { "smart",          Publishing.PublishManager.PublishSmart },
-                    { "incremental",    Publishing.PublishManager.PublishIncremental }
-                };
-        }
+		public PublishService()
+		{
+			_publishingActions = new Dictionary<string, Func<Database, Database[], Language[], Handle>>
+				{
+					{ "full",           Publishing.PublishManager.Republish },
+					{ "smart",          Publishing.PublishManager.PublishSmart },
+					{ "incremental",    Publishing.PublishManager.PublishIncremental }
+				};
+		}
 
 		public void Run(ItemsToPublish itemsToPublish)
 		{
@@ -55,45 +55,45 @@ namespace Sitecore.Ship.Infrastructure
 		}
 
 		public void Run(PublishParameters publishParameters)
-        {
-            var publishingMode = publishParameters.Mode.ToLower();
+		{
+			var publishingMode = publishParameters.Mode.ToLower();
 
-            if (!_publishingActions.ContainsKey(publishingMode))
-            {
-                throw new InvalidOperationException(string.Format("Invalid publishing mode ({0})", publishingMode));
-            }
+			if (!_publishingActions.ContainsKey(publishingMode))
+			{
+				throw new InvalidOperationException(string.Format("Invalid publishing mode ({0})", publishingMode));
+			}
 
-            PublishingTask(_publishingActions[publishingMode], publishParameters);
-        }
+			PublishingTask(_publishingActions[publishingMode], publishParameters);
+		}
 
-        public DateTime GetLastCompletedRun(PublishLastCompleted completeParameters)
-        {
-            // please note http://stackoverflow.com/questions/12416141/get-the-date-time-that-sitecore-last-published
+		public DateTime GetLastCompletedRun(PublishLastCompleted completeParameters)
+		{
+			// please note http://stackoverflow.com/questions/12416141/get-the-date-time-that-sitecore-last-published
 
-            var source = Sitecore.Configuration.Factory.GetDatabase(completeParameters.Source);
-            var target = Sitecore.Configuration.Factory.GetDatabase(completeParameters.Target);
-            
-            var language = LanguageManager.GetLanguage(completeParameters.Language);
+			var source = Sitecore.Configuration.Factory.GetDatabase(completeParameters.Source);
+			var target = Sitecore.Configuration.Factory.GetDatabase(completeParameters.Target);
+			
+			var language = LanguageManager.GetLanguage(completeParameters.Language);
 
 
-            Assert.IsNotNull(source, "Source database {0} cannot be found".Formatted(completeParameters.Source));
-            Assert.IsNotNull(source, "Target database {0} cannot be found".Formatted(completeParameters.Target));
-            Assert.IsNotNull(language, "Language {0} cannot be found".Formatted(completeParameters.Language));
+			Assert.IsNotNull(source, "Source database {0} cannot be found".Formatted(completeParameters.Source));
+			Assert.IsNotNull(source, "Target database {0} cannot be found".Formatted(completeParameters.Target));
+			Assert.IsNotNull(language, "Language {0} cannot be found".Formatted(completeParameters.Language));
 
-            var date = source.Properties.GetLastPublishDate(target, language);
-            return date;
-        }
+			var date = source.Properties.GetLastPublishDate(target, language);
+			return date;
+		}
 
-        private static void PublishingTask(Func<Database, Database[], Language[], Handle> publishType, PublishParameters publishParameters)
-        {
-            using (new SecurityModel.SecurityDisabler())
-            {
-                var master = Sitecore.Configuration.Factory.GetDatabase(publishParameters.Source);
-                var targetDBs = publishParameters.Targets.Select(Sitecore.Configuration.Factory.GetDatabase).ToArray();
-                var languages = publishParameters.Languages.Select(LanguageManager.GetLanguage).ToArray();
+		private static void PublishingTask(Func<Database, Database[], Language[], Handle> publishType, PublishParameters publishParameters)
+		{
+			using (new SecurityModel.SecurityDisabler())
+			{
+				var master = Sitecore.Configuration.Factory.GetDatabase(publishParameters.Source);
+				var targetDBs = publishParameters.Targets.Select(Sitecore.Configuration.Factory.GetDatabase).ToArray();
+				var languages = publishParameters.Languages.Select(LanguageManager.GetLanguage).ToArray();
 
-                publishType(master, targetDBs, languages);
-            }
-        }
-    }
+				publishType(master, targetDBs, languages);
+			}
+		}
+	}
 }
