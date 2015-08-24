d7d342cd1969cf42573e69a6067b8bd289473d5e
Kevin Obee <kevin.obee@gmail.com>
2015-08-24 01:36:53 +0100

    Minor change to ItemsToPublish to make it safe to use by default

--- src/Sitecore.Ship.Core/Domain/ItemsToPublish.cs
+++ src/Sitecore.Ship.Core/Domain/ItemsToPublish.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 
 namespace Sitecore.Ship.Core.Domain
 {
-    public class ItemsToPublish
-    {
-        public List<Guid> Items { get; set; }
-        public string[] TargetDatabases { get; set; }
-        public string[] TargetLanguages { get; set; }
+	public class ItemsToPublish
+	{
+		public List<Guid> Items { get; set; }
+		public string[] TargetDatabases { get; set; }
+		public string[] TargetLanguages { get; set; }
 
-        public ItemsToPublish()
-        {
-          Items = new List<Guid>();
-        }
-    }
+		public ItemsToPublish()
+		{
+			Items = new List<Guid>();
+			TargetDatabases = new string[] { };
+			TargetLanguages = new string[] { };
+		}
+	}
 }
