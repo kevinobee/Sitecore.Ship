6657776da5fa909310d8174f3395b59f2cfb3c0d
Kevin Obee <kevin.obee@gmail.com>
2016-07-17 22:48:35 +0100

    Command line build updated to implement Build, Test, Analyze, Package and ValidateServices task entry points

--- build.cmd
+++ build.cmd
@@ -1,10 +1,23 @@
 @echo Off
+cd %~dp0
+
+SETLOCAL
+
 set target=%1
 if "%target%" == "" (
-   set target=Go
+   set target=Full
 )
 set config=%2
 if "%config%" == "" (
    set config=Debug
 )
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+git rev-parse HEAD > sha.txt
+for /f "delims=" %%a in (sha.txt) do set SHA=%%a
+
+set NuGetExe=nuget
+nuget restore
+
+msbuild Build\Build.proj /t:"%target%" /p:Configuration="%config%" /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+git checkout src\Common\CommonVersionInfo.cs
--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -3,16 +3,11 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyCompany("Sitecore")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
-
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("StoneSoupDeveloper.com")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("Sitecore.Ship")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
-
 [assembly: NeutralResourcesLanguage("en-GB")]
